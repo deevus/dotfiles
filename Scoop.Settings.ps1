6e2dabca28cb4cae365bf931ac18f848357aae51
Simon Hartcher <deevus@gmail.com>
2015-02-06 17:49:06 +1100

    Changed to use busybox by default over coreutils
    Moved busybox to the end of the apps array to force
    busybox as the default shims

--- Scoop.Settings.ps1
+++ Scoop.Settings.ps1
@@ -1,6 +1,7 @@
 #list of basic apps to install from scoop
-$apps = "openssh", "busybox", "coreutils", "sysinternals", "git-flow",
-  "touch", "less", "grep", "vim", "sudo", "sed", "shim", "oneget"
+$apps = "openssh", "coreutils", "sysinternals", "git-flow",
+  "touch", "less", "grep", "vim", "sudo", "sed", "shim", "oneget", 
+  "busybox"
 
 #concfg theme
 $theme = "monokai"
