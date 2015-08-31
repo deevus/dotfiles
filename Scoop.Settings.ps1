6bbbe35f58c97aeb0b2171af1e3d0b2a42fbe571
Simon Hartcher <simon@simonhartcher.com>
2015-09-01 08:59:37 +1000

    Add nodejs to scoop apps

--- Scoop.Settings.ps1
+++ Scoop.Settings.ps1
@@ -1,7 +1,7 @@
 #list of basic apps to install from scoop
 $apps = "openssh", "coreutils", "sysinternals", "git", "git-flow",
   "touch", "less", "grep", "vim", "sudo", "sed", "shim", "busybox",
-  "atom", "emacs", "axel", "curl", "wget", "conemu"
+  "atom", "emacs", "axel", "curl", "wget", "conemu", "nodejs"
 
 $globalApps = "7zip"
 
