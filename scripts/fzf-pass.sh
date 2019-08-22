7f1c3d2462ac20d0e021a4083731e7303d4f8519
Simon Hartcher <simon@simonhartcher.com>
2019-08-23 09:49:29 +1000

    Change fzf-pass username field to login

--- scripts/fzf-pass.sh
+++ scripts/fzf-pass.sh
@@ -3,7 +3,7 @@ cd $HOME/.password-store
 PASSFILE=$(tree -Ffi | grep '.gpg' | sed 's/.gpg$//g' | sed 's/^..//' | fzf)
 
 if [ -z "$PASSFILE" ]; then
-	exit 0
+    exit 0
 fi
 
 RESP=$(cat <<EOF | fzf
@@ -18,31 +18,31 @@ EOF
 swaymsg move container to workspace 9
 
 case "$RESP" in
-	Autotype)
-		# type username
-		ydotool type "$(pass $PASSFILE | grep username: | cut -d' ' -f2-)"
-		# press tab
-		ydotool key Tab
-		# type password
-		ydotool type "$(pass $PASSFILE | head -n 1)"
-		# press enter
-		ydotool key Enter
-		;;
-	Username)
-		ydotool type "$(pass $PASSFILE | grep username: | cut -d' ' -f2-)"
-		;;
-	Password)
-		# type password
-		ydotool type "$(pass $PASSFILE | head -n 1)"
-		;;
-	OTP)
-		# type OTP
-		ydotool type "$(pass otp $PASSFILE)"
-		;;
-	URL)
-		# type url
-		ydotool type "$(pass $PASSFILE | grep url: | cut -d' ' -f2-)"
-		;;
-	*)
-		exit 1
+    Autotype)
+        # type username
+        ydotool type "$(pass $PASSFILE | grep login: | cut -d' ' -f2-)"
+        # press tab
+        ydotool key Tab
+        # type password
+        ydotool type "$(pass $PASSFILE | head -n 1)"
+        # press enter
+        ydotool key Enter
+        ;;
+    Username)
+        ydotool type "$(pass $PASSFILE | grep login: | cut -d' ' -f2-)"
+        ;;
+    Password)
+        # type password
+        ydotool type "$(pass $PASSFILE | head -n 1)"
+        ;;
+    OTP)
+        # type OTP
+        ydotool type "$(pass otp $PASSFILE)"
+        ;;
+    URL)
+        # type url
+        ydotool type "$(pass $PASSFILE | grep url: | cut -d' ' -f2-)"
+        ;;
+    *)
+        exit 1
 esac
