b0eb4e6dfa257d7716a80777be89111e6b492b76
Simon Hartcher <simon@simonhartcher.com>
2023-05-26 10:47:12 +1000

    Add .config/nvim/lua/set.lua

---
+++ dot_config/nvim/lua/set.lua
@@ -0,0 +1,40 @@
+-- options vim.api.nvim_set_option
+vim.o.number = true
+vim.o.background = 'dark'
+vim.o.laststatus = 2 -- always show statusbar
+
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.cache/nvim/undo"
+vim.o.undofile = true
+
+vim.go.t_Co = '256'
+vim.o.hlsearch = true
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.smartindent = true
+vim.o.autoindent = true
+
+-- disable powerline as it is not working with neovim 0.5
+vim.g.powerline_loaded = 1
+
+if (vim.api.nvim_eval('has("multi_byte")')) then
+  vim.o.encoding = 'utf-8'
+  vim.g.fileencoding = 'utf-8'
+  vim.g.fileencodings = 'ucs-bom,utf-8,latin1'
+end
+
+vim.o.termguicolors = true
+
+-- TODO: move to colors.lua
+vim.cmd.colorscheme("dracula")
+
+if (vim.api.nvim_eval('&diff') > 0) then
+  vim.api.nvim_exec([[ colorscheme github ]], true)
+end
+-- end
+
+vim.o.secure = true
+vim.o.compatible = false
+
