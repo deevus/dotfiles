074f3521b995e53d22327df589cbce30c6ab59ae
Simon Hartcher <simon@simonhartcher.com>
2023-05-26 10:47:03 +1000

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,56 +1,18 @@
 require 'packages'
 require 'lsp'
+require 'set'
 
--- options vim.api.nvim_set_option
-vim.o.number = true
-vim.o.background = 'dark'
-vim.o.laststatus = 2 -- always show statusbar
-vim.o.backup = true
-vim.go.t_Co = '256'
-vim.o.hlsearch = true
+local autocmd = vim.api.nvim_create_autocmd
 
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
+autocmd({"BufRead", "BufWritePre"}, {
+    pattern = {"*.sh"},
+    command = "normal gg=G"
+})
 
--- disable powerline as it is not working with neovim 0.5
-vim.g.powerline_loaded = 1
-
-if (vim.api.nvim_eval('has("multi_byte")')) then
-  vim.o.encoding = 'utf-8'
-  vim.g.fileencoding = 'utf-8'
-  vim.g.fileencodings = 'ucs-bom,utf-8,latin1'
-end
-
-vim.o.termguicolors = true
-
-vim.api.nvim_exec([[
-  " For all text files set 'textwidth' to 78 characters.
-  autocmd filetype text setlocal textwidth=78
-
-  filetype indent on
-  filetype plugin on
-
-  autocmd BufRead,BufWritePre *.sh normal gg=G
-
-  colorscheme dracula
-
-  setglobal autoindent smartindent
-]], true)
-
-if (vim.api.nvim_eval('&diff') > 0) then
-  vim.api.nvim_exec([[ colorscheme github ]], true)
-end
-
-vim.o.compatible = false
-
--- global vars vim.api.nvim_set_var
-
-vim.api.nvim_exec([[
-  runtime _bindings.vim
-]], true)
-
-vim.o.secure = true
+autocmd({"filetype"}, {
+    pattern = {"text"},
+    command = "setlocal textwidth=78"
+})
 
 -- save/load folds automatically
 vim.api.nvim_exec([[
@@ -61,15 +23,9 @@ vim.api.nvim_exec([[
   augroup END
 ]], true)
 
--- undofile
 vim.api.nvim_exec([[
-  if !isdirectory($HOME."/.cache/nvim/undo")
-      call mkdir($HOME."/.cache/nvim/undo", "-p", 0700)
-  endif
-  set undodir=~/.cache/nvim/undo
-  set undofile
+  runtime _bindings.vim
 ]], true)
-
 
 require '_telescope'
 require 'theme'
