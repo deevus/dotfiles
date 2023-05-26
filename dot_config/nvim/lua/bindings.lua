8b838afcab6b75655fc097321231fa6536aed4d9
Simon Hartcher <simon@simonhartcher.com>
2023-05-26 10:01:55 +1000

    Update .config/nvim/lua/bindings.lua

--- dot_config/nvim/lua/bindings.lua
+++ dot_config/nvim/lua/bindings.lua
@@ -1,46 +1,43 @@
-local vimp = require('vimp')
-local opts = { silent=true }
-
 local telescope = require 'telescope.builtin'
 local extensions = require 'telescope'.extensions
 local projects = extensions.projects
 
-vimp.nnoremap(opts, '<C-p>', function() 
+vim.keymap.set("n", "<C-p>", function()
   telescope.find_files()
 end)
 
-vimp.nnoremap(opts, '<C-o>', function() 
+vim.keymap.set("n", '<C-o>', function() 
   projects.find_project_files()
 end)
 
-vimp.nnoremap(opts, '<leader>p', function() 
+vim.keymap.set("n", '<leader>p', function() 
   telescope.find_files()
 end)
 
-vimp.nnoremap(opts, '<leader>g', function() 
+vim.keymap.set("n", '<leader>g', function() 
   telescope.live_grep()
 end)
 
-vimp.nnoremap(opts, '<leader>b', function() 
+vim.keymap.set("n", '<leader>b', function() 
   telescope.buffers()
 end)
 
-vimp.nnoremap(opts, '<leader>s', '<cmd>FzfPreviewBufferTags<cr>')
-vimp.nnoremap(opts, '<leader>B', '<cmd>FzfPreviewAllBuffers<cr>')
-vimp.nnoremap(opts, '<leader>l', '<cmd>FzfPreviewLines<cr>')
-vimp.nnoremap(opts, '<leader>R', '<cmd>FzfPreviewProjectMruFiles<cr>')
-vimp.nnoremap(opts, '<leader>q', '<cmd>FzfPreviewQuickFix<cr>')
-vimp.nnoremap(opts, '<leader>r', '<cmd>set relativenumber!<cr>')
+vim.keymap.set("n", '<leader>s', '<cmd>FzfPreviewBufferTags<cr>')
+vim.keymap.set("n", '<leader>B', '<cmd>FzfPreviewAllBuffers<cr>')
+vim.keymap.set("n", '<leader>l', '<cmd>FzfPreviewLines<cr>')
+vim.keymap.set("n", '<leader>R', '<cmd>FzfPreviewProjectMruFiles<cr>')
+vim.keymap.set("n", '<leader>q', '<cmd>FzfPreviewQuickFix<cr>')
+vim.keymap.set("n", '<leader>r', '<cmd>set relativenumber!<cr>')
 
-vimp.nnoremap(opts, '<Leader>ta', function()
+vim.keymap.set("n", '<Leader>ta', function()
   extensions.vstask.tasks()
 end)
-vimp.nnoremap(opts, '<Leader>ti', function()
+vim.keymap.set("n", '<Leader>ti', function()
   extensions.vstask.inputs()
 end)
-vimp.nnoremap(opts, '<Leader>th', function()
+vim.keymap.set("n", '<Leader>th', function()
   extensions.vstask.history()
 end)
-vimp.nnoremap(opts, '<Leader>tl', function()
+vim.keymap.set("n", '<Leader>tl', function()
   extensions.vstask.launch()
 end)
