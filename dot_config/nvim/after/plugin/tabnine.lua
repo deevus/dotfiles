b2efa2519482c16f277c3cce17397278769e0c18
Simon Hartcher <simon@simonhartcher.com>
2023-09-05 09:36:52 +1000

    Update .config/nvim/after/plugin/tabnine.lua

--- dot_config/nvim/after/plugin/tabnine.lua
+++ dot_config/nvim/after/plugin/tabnine.lua
@@ -1,9 +1,13 @@
-require('tabnine').setup({
-  disable_auto_comment=true,
-  accept_keymap="<C-M>",
-  dismiss_keymap = "<C-]>",
-  debounce_ms = 800,
-  suggestion_color = {gui = "#808080", cterm = 244},
-  exclude_filetypes = {"TelescopePrompt"},
-  log_file_path = nil, -- absolute path to Tabnine log file
-})
+local tabnine = require('tabnine')
+
+if (tabnine) then
+  tabnine.setup({
+    disable_auto_comment=true,
+    accept_keymap="<C-M>",
+    dismiss_keymap = "<C-]>",
+    debounce_ms = 800,
+    suggestion_color = {gui = "#808080", cterm = 244},
+    exclude_filetypes = {"TelescopePrompt"},
+    log_file_path = nil, -- absolute path to Tabnine log file
+  })
+end
